58ca7d3d70571138f799c24c55c0eb5ec2a29335
Vincent Orsini <vao2002@gmail.com>
2021-04-09 20:30:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,44 @@
 function basicTeenager(age) {
-
+    if(age>=13&&age<=19){
+      console.log("You are a teenager!");
+    }
+    else if !(age>=13&&age<=19) {
+      return undefined;
+    }
 }
 
 function teenager(age) {
-
+  if(age>=13&&age<=19){
+    console.log("You are a teenager!");
+  }
+  else{
+    console.log("You are not a teenager");
+  }
 }
 
 function ageChecker(age) {
-
+  if(age>=13&&age<=19){
+    console.log("You are a teenager!");
+  }
+  else if (age<=12){
+    console.log("You are a kid");
+  }
+  else if(age>19){
+    console.log("You are a grownup");
+  }
 }
 
 function ternaryTeenager(age) {
-
+    return if (age>=13&&age<=19)?
+      console.log("You are a teenager"):
+    console.log("You are not a teenager")
 }
 
 function switchAge(age) {
-
+    switch(age){
+      case (age>=13&&age<=19) :
+         return "You are a teenager"
+      default:
+         return "You have an age"
+    }
 }
